a217d6808214ba8f1e41ea3f9bc21b855dd1b004
System Administrator <root@500010352135-nb.corp1.rc.itau>
2019-09-04 12:36:41 -0300

    Make auth component for login

---
+++ src/app/api/api/login.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { RequestService } from '../request.service';
+import { StorageService } from '../storage/storage.service';
+
+@Injectable()
+export class ApiLoginService {
+  constructor(
+    private requestService: RequestService
+  ) { }
+
+  async makeLogin(email: string, password: string) {
+    const result = await this.requestService.default('/auth/login', true, 'post', {
+      email,
+      password
+    });
+    console.log(result);
+  }
+}
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,16 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { ApiLoginService } from '../api/api/login.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  providers: [ApiLoginService]
 })
 export class LoginComponent implements OnInit {
-  focus;
-  focus1;
-  constructor() { }
+  loginForm: FormGroup;
+
+  constructor(
+    private apiLoginService: ApiLoginService,
+    private formBuilder: FormBuilder,
+    private toastr: ToastrService
+  ) { }
 
   ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      password: ['', [Validators.required, Validators.minLength(4)]],
+      email: ['', [Validators.required, Validators.email]],
+    });
+  }
+
+  makeLogin() {
+    const { email, password } = this.loginForm.controls;
+
+    if (this.loginForm.invalid) {
+      const validValue = "VALID";
+      if (email.status != validValue || password.status != validValue) {
+        this.toastr.error('E-mail ou senha incorreto');
+      }
+    } else if (this.loginForm.valid) {
+      this.apiLoginService.makeLogin(email.value, password.value);
+    }
   }
 
 }
